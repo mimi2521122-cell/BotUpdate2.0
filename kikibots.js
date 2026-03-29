9e379431c1ea60f49c15915d93e4bf9bed6b720c
mimi2521122-cell <mimi2521122@gmail.com>
2026-03-29 08:02:32 +0630

    Update kikibots.js

--- kikibots.js
+++ kikibots.js
@@ -6,7 +6,7 @@ const https = require('https');
 const { setTimeout } = require('timers/promises');
 
 const ADMIN_ID = 7308292609;
-const BOT_TOKEN = "8533662019:AAHKsMxXgYH9NmJOwCAwfK-TGsAqi0m-TiM";
+const BOT_TOKEN = "8533662019:AAEreVXvp5o8NLZS47aKrDjZjZXH4c3mp8I";
 const IGNORE_SSL = true;
 const WIN_LOSE_CHECK_INTERVAL = 1000;
 const MAX_RESULT_WAIT_TIME = 60000;
